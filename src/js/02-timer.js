cfdde57725822ca8d7d78ae22cb872bc8ad8344b
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-05 21:50:54 +0200

    first task added

---
+++ src/js/02-timer.js
@@ -0,0 +1,9 @@
+const timerStart =  document.querySelector('button[data-start]');
+const timerSet =  document.querySelector('#datetime-picker');
+
+
+timerStart.addEventListener('click', dateSet)
+
+function dateSet () {
+    
+}
