c4fe61b8afda4bebec4865b7f8d1a958730590f7
SemenZvehintsev <103433501+SemenZvehintsev@users.noreply.github.com>
2022-11-05 20:47:51 +0000

    Deploying to gh-pages from @ SemenZvehintsev/goit-js-hw-09@db8ea51b05a1d347d9bd4177cd17a14ca8264fa4 🚀

---
+++ 01-color-switcher.20c30656.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");function o(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){timer=setInterval(o,1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(timer),e.setAttribute("disabled",!0),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.20c30656.js.map
