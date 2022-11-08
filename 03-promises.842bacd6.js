215a59ee0eeb9d3ed89e5e9c9f7339148725dcf7
SemenZvehintsev <103433501+SemenZvehintsev@users.noreply.github.com>
2022-11-08 14:46:59 +0000

    Deploying to gh-pages from @ SemenZvehintsev/goit-js-hw-09@02d7d352cca78ccd6fc09954b0e91000ac491946 🚀

---
+++ 03-promises.842bacd6.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("a");console.log(o);
+//# sourceMappingURL=03-promises.842bacd6.js.map
---
+++ 03-promises.b7c22231.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("a");console.log(o)}();
+//# sourceMappingURL=03-promises.b7c22231.js.map
