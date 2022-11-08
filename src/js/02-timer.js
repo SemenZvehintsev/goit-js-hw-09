4deb53ab93bdc25dc1d337de8285d5773670adf8
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-08 15:59:08 +0200

    add second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,8 +2,6 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 
-
-
 const startBtn = document.querySelector('button');
 const timerSet = document.querySelector('#datetime-picker');
 const daysLeft = document.querySelector('[data-days]');
@@ -12,9 +10,12 @@ const minutesLeft = document.querySelector('[data-minutes]');
 const secondsLeft = document.querySelector('[data-seconds]');
 
 startBtn.setAttribute("disabled", true);
-startBtn.addEventListener('click', convertMs(ms))
+startBtn.addEventListener('click', timerOn)
 
-const date = new Date()
+let pickTime;
+let date;
+let ms;
+let timerGo;
 
 const options = {
     enableTime: true,
@@ -22,7 +23,6 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
       if (selectedDates[0].getTime() > date.getTime()) {
         startBtn.removeAttribute("disabled");
       } else {
@@ -32,25 +32,40 @@ const options = {
     },
 };
 
-const timer = flatpickr(timerSet, options);
+let timer = flatpickr(timerSet, options);
 
 timerSet.addEventListener('input', (event) => {
-    const pickTime = new Date(event.currentTarget.value)
-    console.log(pickTime.getTime())
-
-    const ms = pickTime.getTime() - date.getTime();
-    // console.log(ms);
-    return ms
-
+    date = new Date();
+    pickTime = new Date(event.currentTarget.value);
 })
 
-function convertMs(ms) {
+function timerOn() {
+  timerGo = setInterval(convertMs, 1000)
+  startBtn.setAttribute("disabled", true);
+  timerSet.setAttribute("disabled", true);
+}
+
+function timerStop() {
+  if (Number(secondsLeft.textContent)
+  === 0 && Number(hoursLeft.textContent)
+  === 0 && Number(minutesLeft.textContent)
+  === 0 && Number(daysLeft.textContent)
+  === 0) {
+    clearInterval(timerGo)
+    timerSet.removeAttribute("disabled");
+  }
+
+}
+
+function convertMs() {
     // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
   
+    date = new Date();
+    ms = pickTime.getTime() - date.getTime();
     // Remaining days
     const days = Math.floor(ms / day);
     // Remaining hours
@@ -59,82 +74,9 @@ function convertMs(ms) {
     const minutes = Math.floor(((ms % day) % hour) / minute);
     // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-
-
-
-
-// class NewTimer {
-
-//     startBtn = document.querySelector('button');
-//     timerSet = document.querySelector('#datetime-picker');
-//     daysLeft = document.querySelector('[data-days]');
-//     hoursLeft = document.querySelector('[data-hours]');
-//     minutesLeft = document.querySelector('[data-minutes]');
-//     secondsLeft = document.querySelector('[data-seconds]');
-    
-//     start() {
-//         this.startBtn.setAttribute("disabled", true);
-//         this.startBtn.addEventListener('click', this.convertMs)
-//         this.timerSet.addEventListener('input', this.calcMs)
-//     }
-        
-//     date = new Date()
-    
-//     options = {
-//         enableTime: true,
-//         time_24hr: true,
-//         defaultDate: new Date(),
-//         minuteIncrement: 1,
-//         onClose(selectedDates) {
-//           console.log(selectedDates[0]);
-//           if (selectedDates[0].getTime() > this.date.getTime()) {
-//             this.startBtn.removeAttribute("disabled");
-//           } else {
-//             this.startBtn.setAttribute("disabled", true);
-//             window.alert("Please choose a date in the future");
-//             }
-//         },
-//     };
-    
-//     timer = flatpickr(this.timerSet, this.options);
-    
-//     calcMs(event) {
-//         const pickTime = new Date(event.currentTarget.value)
-//         console.log(pickTime.getTime())
-    
-//         const ms = pickTime.getTime() - this.date.getTime();
-//         console.log(ms);
-//         return ms
-//     }
-    
-//     convertMs(ms) {
-//         // Number of milliseconds per unit of time
-//         const second = 1000;
-//         const minute = second * 60;
-//         const hour = minute * 60;
-//         const day = hour * 24;
-      
-//         // Remaining days
-//         const days = Math.floor(ms / day);
-//         // Remaining hours
-//         const hours = Math.floor((ms % day) / hour);
-//         // Remaining minutes
-//         const minutes = Math.floor(((ms % day) % hour) / minute);
-//         // Remaining seconds
-//         const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-//         return { days, hours, minutes, seconds };
-//       }
-      
-    
-
-
-    
-// }
-
-
-// new NewTimer().start()
+    daysLeft.textContent = days;
+    hoursLeft.textContent = hours;
+    minutesLeft.textContent = minutes;
+    secondsLeft.textContent = seconds;
+    timerStop()
+}
