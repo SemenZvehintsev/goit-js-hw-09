a7f6a00c8b8f6baeb8d3b28176fd508167d7a1f2
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-09 11:03:17 +0200

    add start JS at 03 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,27 @@
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
+const inputDelay = document.querySelector('[name="delay"]');
+const inputStep = document.querySelector('[name="step"]');
+const inputAmount = document.querySelector('[name="amount"]');
+const submitBtn = document.querySelector('button');
+const fullForm = document.querySelector('form')
 
-
-const inputDelay = document.querySelector('form')
-
-
-console.log(inputDelay)
-
-
-
-
-
-
-
-
-
+console.log(inputDelay);
+console.log(inputStep);
+console.log(inputAmount);
+console.log(submitBtn);
+console.log(fullForm);
 
 
 
 
 
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
